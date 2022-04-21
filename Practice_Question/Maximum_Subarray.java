24387d93274409bb591d0bddcf711a9b211386e0
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-04-21 23:42:56 +0530

    Maximum Subarray

---
+++ Practice_Question/Maximum_Subarray.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/maximum-subarray/
+
+
+
+class Solution {
+    public int maxSubArray(int[] arr) {
+        int maxsum = arr[0];
+      int  currsum = maxsum;
+        for(int i=1;i<arr.length;i++){
+            currsum = Math.max(arr[i],arr[i]+currsum);
+        maxsum = Math.max(maxsum,currsum);
+        }
+        return maxsum;
+    }
+   
+}
