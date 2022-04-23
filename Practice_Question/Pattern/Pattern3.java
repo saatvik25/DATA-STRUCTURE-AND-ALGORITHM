188f1c7cce89e7a48ea79971c9acc99d093d3ad4
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-04-24 00:04:02 +0530

    Pattern 3

---
+++ Practice_Question/Pattern/Pattern3.java
@@ -0,0 +1,22 @@
+
+public class pattern {
+    public static void main(String args[]){
+
+        pattern(5);
+        //*****
+        //****
+        //***
+        //**
+        //*
+
+    }
+
+    public static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
