aefcf71842d1b83292b50530aa330621cd450ff1
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-04-23 23:27:25 +0530

    Pattern 2

---
+++ Practice_Question/Pattern/Pattern2.java
@@ -0,0 +1,22 @@
+
+public class pattern {
+    public static void main(String args[]){
+
+        pattern1(5);
+         //*****
+        //*****
+        //*****
+        //*****
+        //*****
+
+    }
+
+    public static void pattern1(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
