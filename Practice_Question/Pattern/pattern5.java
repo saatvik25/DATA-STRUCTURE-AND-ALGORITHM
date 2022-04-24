20cf97a148fe412a637275b1d1e0506338c1a2b2
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-04-24 23:31:38 +0530

    pattern 5

---
+++ Practice_Question/Pattern/pattern5.java
@@ -0,0 +1,32 @@
+
+public class pattern {
+    public static void main(String args[]){
+
+        pattern(5);
+        //*
+        //* *
+        //* * *
+        //* * * *
+        //* * * * *
+        //* * *
+        //* *
+        //*
+
+
+    }
+
+    public static void pattern(int n){
+        for(int i=1;i<= (2*n-1) ;i++) {
+            int col;
+            if (i > n) {
+                col = n - (i - n + 1);
+            } else {
+                col=i;
+            }
+            for (int j = 1; j <=col; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
