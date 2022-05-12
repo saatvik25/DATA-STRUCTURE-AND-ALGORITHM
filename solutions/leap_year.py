de02dd29f69e4807f571bbb750071110a27550b4
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-05-12 19:11:54 +0530

    Write a function

---
+++ solutions/leap_year.py
@@ -0,0 +1,20 @@
+
+
+
+    leap = False
+    
+    # Write your logic here
+    if(year%4 == 0):
+        if(year%100 ==0):
+            if(year%400 ==0):
+                leap = True
+            else:
+                False
+        else:
+            leap = True 
+    else:
+        leap = False
+                               
+    return leap
+
+year = int(input())
