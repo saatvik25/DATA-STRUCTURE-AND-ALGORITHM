76e3aabfef2f09b0d325120885a5d34fb8532a80
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-06-06 23:15:20 +0530

    Print Function

---
+++ solutions/Print Function.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        # The end parameter basically prints after all the output objects present in one output statement have been returned
+        print(i,end ='')
+
