a079337a3f4e89c37802472a3c3c5be5902f822a
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-06-07 15:14:16 +0530

    validate bst

---
+++ Practice_Question/validate_bst.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isbst(root , Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    boolean isbst(TreeNode root ,long min,long max){
+        if(root ==null){
+            return true;
+        }
+        if(root.val >min && root.val<max){
+            boolean left = isbst(root.left,min,root.val);
+            boolean right = isbst(root.right , root.val , max);
+            return left && right;
+        }
+        else{
+            return false;
+        }
+    }
+}
