79291f88544384219431ae2d3c4b407267ad2de3
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-09-07 21:14:19 +0530

    Plus One

---
+++ LEETCODE SOLUTIONS/Plus One.java
@@ -0,0 +1,24 @@
+
+
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i =n-1;i>=0;i--){
+        if(digits[i] != 9){
+            digits[i] =  digits[i]+1;
+            break;
+        }
+        else{
+            digits[i] =0; 
+            
+        }
+    }
+        if(digits[0] == 0){
+            int temp[] = new int[n+1];
+            temp[0] = 1;
+            return temp;
+        }
+        return digits;
+    }
+}
