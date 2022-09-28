94c1e6c2050f59950244ef6575f3826471107f06
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-09-28 22:27:56 +0530

    print name n times

---
+++ Practice_Question/recursion/name.java
@@ -0,0 +1,17 @@
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    name(5);
+
+	}
+	static void name(int n){
+	    if(n==0){
+	        return;
+	    }
+	    System.out.println("saatvik");
+	    name(n-1);
+	}
+}
