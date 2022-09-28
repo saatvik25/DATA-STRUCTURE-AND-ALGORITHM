2324917d98df7197cd3d483b95c5bacc02d392f9
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-09-28 23:29:15 +0530

    functional

---
+++ Practice_Question/recursion/functional.java
@@ -0,0 +1,17 @@
+// add in a function
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	   System.out.println(func(5));
+
+	}
+	static int func(int n){
+	    if(n==0){
+	        return 0;
+	    }
+      return n+ func(n-1);
+	    	}
+}
