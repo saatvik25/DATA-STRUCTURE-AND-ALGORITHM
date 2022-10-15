2ac0501f4d2771a1eba5942e833e8d302c637cb3
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-10-15 22:23:43 +0530

    Sum of distinct elements

---
+++ gfg solution/Sum.java
@@ -0,0 +1,19 @@
+
+
+
+class Solution {
+    int findSum(int arr[], int n) {
+        // code here
+        HashSet<Integer> s = new HashSet<>();
+        int sum =0;
+        for(int i =0;i<n;i++){
+            if(!s.contains(arr[i])){
+                sum = sum + arr[i];
+                s.add(arr[i]);
+            }
+            
+        }
+    
+        return sum;
+    }
+}
