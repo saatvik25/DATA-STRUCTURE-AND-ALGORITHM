81a4509c6fbf2d93b89e04d975662f1acc91a382
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-10-26 17:32:51 +0530

    Number of occurrence

---
+++ gfg solution/solution.java
@@ -0,0 +1,16 @@
+
+
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int res =0;
+        int count =0;
+        for(int i =0;i<n;i++){
+            if(arr[i]== x){
+                count++;
+            }
+        }
+        return count;
+    }
+}
