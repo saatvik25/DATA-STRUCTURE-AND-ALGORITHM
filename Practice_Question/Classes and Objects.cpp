747999ffa811b1606f9bc07231abc4824d71e5e3
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-11-01 23:38:34 +0530

    Classes and Objects

---
+++ Practice_Question/Classes and Objects.cpp
@@ -0,0 +1,17 @@
+
+
+
+class Student {
+    int totalScore = 0;
+    public:.cP
+    void input() {
+        for (int i = 0; i < 5; i++) {
+            int x;
+            cin >> x;
+            totalScore += x;
+        }
+    }
+    int calculateTotalScore() {
+        return totalScore;
+    }   
+};
