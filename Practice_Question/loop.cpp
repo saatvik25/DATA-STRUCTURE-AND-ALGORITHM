653c11eb131048b0e78243bc17bd1549f920cb1a
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-11-08 00:20:04 +0530

    For Loop

---
+++ Practice_Question/loop.cpp
@@ -0,0 +1,20 @@
+
+
+
+int main() {
+    // Complete the code.
+    string num[11] = { "one", "two", "three", "four", "five", "six", "seven", "eight",                         "nine","even","odd"};
+int i,a,b;
+cin>>a>>b;
+for(int i =a;i<=b;i++){
+    if(i>9 && i%2 == 0){
+        cout<<num[9]<<endl;
+    }else if(i>9 && i%2 != 0){
+        cout<<num[10]<<endl;
+    }else{
+        cout<<num[i-1]<<endl;
+    }
+    
+}
+    return 0;
+}
