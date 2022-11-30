9a79344f43c3edeb4dad6bd923186562e3ac53c9
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-12-01 00:20:11 +0530

    Smallest Range I

---
+++ LEETCODE SOLUTIONS/908.cpp
@@ -0,0 +1,22 @@
+
+
+
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+       int  mi = INT_MAX;
+        int ma = 0;
+       for(int i=0;i<nums.size();i++){
+           mi = min(mi,nums[i]);
+           ma = max(ma , nums[i]);
+       } 
+        mi = mi+k;
+        ma = ma-k;
+        if(mi>ma){     // agar mi bada hai to 0 return kardo
+            return 0;
+        }
+        else{
+            return ma-mi;
+        }
+    }
+};
