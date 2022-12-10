39608aecc8ef07bf3e7b9dd3e8045b1a02ef2391
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-12-10 09:27:51 +0530

    Update Maximum_Subarray.java

--- Practice_Question/Maximum_Subarray.java
+++ Practice_Question/Maximum_Subarray.java
@@ -1,15 +1,3 @@
 //https://leetcode.com/problems/maximum-subarray/
 
 
-lass Solution {
-    public int maxSubArray(int[] arr) {
-        int maxsum = arr[0];
-      int  currsum = maxsum;
-        for(int i=1;i<arr.length;i++){
-            currsum = Math.max(arr[i],arr[i]+currsum);
-        maxsum = Math.max(maxsum,currsum);
-        }
-        return maxsum;
-    }
-   
-}
