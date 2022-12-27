e8b05b0bdba45ce5f9217436a3949c3bf126e8cf
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-12-28 02:36:17 +0530

    Ransom Note

---
+++ LEETCODE SOLUTIONS/383.cpp
@@ -0,0 +1,20 @@
+
+
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> m ;
+
+        for(auto x : magazine){
+            m[x]++;
+        }
+
+        for(auto i : ransomNote){
+            if(!m[i]--){
+                return false;
+            }
+        }
+        return true;
+    }
+};
