1a1c3b3728217d4fcdb14ab9f58490c2bf5514c7
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-01-02 00:10:18 +0530

    First Bad Version

---
+++ LEETCODE SOLUTIONS/278.cpp
@@ -0,0 +1,18 @@
+
+
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int st = 1,end = n;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            if(isBadVersion(mid) == false) st = mid+1;
+            else end = mid-1;
+        }
+        return st;
+    }
+};
