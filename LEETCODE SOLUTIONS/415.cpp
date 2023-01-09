a0e21ce91aa0f00cedf2ac2064830757c457da8a
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-01-09 13:58:18 +0530

    Add Strings

---
+++ LEETCODE SOLUTIONS/415.cpp
@@ -0,0 +1,40 @@
+
+
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int len1 = num1.length()-1;
+        int len2 = num2.length()-1;
+        int sum =0;
+        int lastdigit ;
+        int carry=0;
+        string ans;
+
+        while(len1>=0 || len2>=0){
+            sum = carry;
+            lastdigit = 0;
+            if(len1>=0){
+                sum = sum+ (num1[len1]- 48);
+                len1--;
+            }
+
+             if(len2>=0){
+                sum = sum+ (num2[len2]- 48);
+                len2--;
+            }
+            lastdigit = sum%10;
+            carry = sum/10;
+            ans.push_back(lastdigit+48);
+
+        }
+            if(carry)
+        {
+            ans.push_back(carry+48);
+        }
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+
+};
