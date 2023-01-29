47573aa5fa1a126e68a132052ed93db8cff785b6
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-01-29 10:48:14 +0530

    Maximize Sum Of Array After K Negations

---
+++ LEETCODE SOLUTIONS/1005.cpp
@@ -0,0 +1,24 @@
+
+
+
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        int sum=0;
+        // How to create a min heap for the priority queue?
+        // priority_queue <int, vector<int>, greater<int>> gq;
+        // for min heap we use this
+        priority_queue <int ,vector<int>,greater<int> > pq(nums.begin(), nums.end());
+        while(k--){
+            int x = pq.top();
+            pq.pop();
+            pq.push(-1* x);
+        }
+        while(!pq.empty()){
+            sum = sum+pq.top();
+            pq.pop();
+
+        }
+        return sum;
+    }
+};
