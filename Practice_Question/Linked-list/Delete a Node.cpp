aa42691b7836a7d73fd6c50a758871e38049ccff
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-02-21 21:49:52 +0530

    Delete a Node

---
+++ Practice_Question/Linked-list/Delete a Node.cpp
@@ -0,0 +1,16 @@
+
+
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+    
+if(position == 0){
+    llist = llist->next;
+    return llist;
+}
+SinglyLinkedListNode* temp = llist;
+for(int i=0;i<position-1;i++){
+temp = temp->next;
+}
+temp->next = temp->next->next;
+return llist;
+}
