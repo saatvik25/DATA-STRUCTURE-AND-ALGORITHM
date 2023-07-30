c68629de4e19aafc8dd6779e56e71805bb2d5e59
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-07-30 13:19:08 +0530

    Split a String in Balanced Strings

---
+++ LEETCODE SOLUTIONS/1221.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int len = s.size();
+        int rcnt =0;
+        int lcnt=0;
+        int i=0;
+        int cnt =0;
+        while(i<len){
+            if(s[i] == 'R'){
+                rcnt++;
+                
+            }else{
+                lcnt++;
+
+            }
+            i++;
+
+             if(lcnt ==rcnt ){
+             cnt++;
+        }
+        }
+       
+        return cnt;
+    }
+};
