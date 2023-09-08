11083f4797053b74585b24142dec00f3a746898e
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-09-08 20:55:47 +0530

    Create Subarray with given XOR

---
+++ MAQ/Subarray with given XOR.cpp
@@ -0,0 +1,36 @@
+
+
+
+Given an array of integers A and an integer B.
+
+Find the total number of subarrays having bitwise XOR of all elements equals to B.
+
+
+
+Problem Constraints
+1 <= length of the array <= 105
+
+1 <= A[i], B <= 109
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+int Solution::solve(vector<int> &A, int B) {
+    map<int,int>mp;
+    int xorr =0;
+    int count =0;
+    
+    for(auto i : A){
+        xorr = xorr^i;
+    
+    if(xorr == B)
+        count++;
+    
+    if(mp.find(xorr^B)!=mp.end())
+        count += mp[xorr^B] ;
+    
+    mp[xorr] +=1;
+    
+    }
+    return count;
+}
