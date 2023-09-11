cedc79fb9aba0c5fc1fd1ba789eb99b8fd1470ad
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2023-09-12 00:10:52 +0530

    Remove Duplicates from Sorted List

---
+++ Practice/83.cpp
@@ -0,0 +1,22 @@
+
+
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+        if(head == NULL){
+            return NULL;
+        }
+        // next check karo nahi ti null pointer error dega
+        while(temp->next != NULL){
+           
+            if(temp->val == temp->next->val){
+                temp->next = temp->next->next;
+            }else{
+             temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
